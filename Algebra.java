0458a05398f57b8ca59fb2409732df29bc22ee73
Bar Manor <bar.manor01@post.runi.ac.il>
2025-11-14 10:45:32 +0200

    after running the tests on all programs #1

--- Algebra.java
+++ Algebra.java
@@ -6,62 +6,164 @@
 public class Algebra {
 	public static void main(String args[]) {
 	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
-	    System.out.println(minus(7,2));  // 7 - 2
-   		System.out.println(minus(2,7));  // 2 - 7
- 		System.out.println(times(3,4));  // 3 * 4
-   		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
-   		System.out.println(pow(5,3));      // 5^3
-   		System.out.println(pow(3,5));      // 3^5
+	    System.out.println(plus(2,3)); 
+		System.out.println(plus(2, -3));
+		System.out.println(plus(-2, 3));
+		System.out.println(plus(-2, -3));  // 2 + -3
+	    System.out.println(minus(2,3)); 
+		System.out.println(minus(2, -3));
+		System.out.println(minus(-2, 3));
+		System.out.println(minus(-2, -3)); // 2 - 7
+ 		System.out.println(times(2,3)); 
+		System.out.println(times(2, -3));
+		System.out.println(times(-2, 3));
+		System.out.println(times(-2, -3));  // 2 + 4 * 2
+   		System.out.println(pow(2,3)); 
+		System.out.println(pow(-2, 3));
    		System.out.println(div(12,3));   // 12 / 3    
-   		System.out.println(div(5,5));    // 5 / 5  
-   		System.out.println(div(25,7));   // 25 / 7
-   		System.out.println(mod(25,7));   // 25 % 7
-   		System.out.println(mod(120,6));  // 120 % 6    
+   		System.out.println(div(5,5)); 
+		System.out.println(div(5,0));    // 5 / 0
+		System.out.println(div(5,-1));    // 5 / 1
+   		System.out.println(div(-25,7));
+   		System.out.println(div(-25,-7));
+	    System.out.println(mod(25,25));   // 25 % 25
+   		System.out.println(mod(120,6));
+		System.out.println(mod(25,7));  // 25 % 7
    		System.out.println(sqrt(36));
+		System.out.println(sqrt(1));
+		System.out.println(sqrt(0));
 		System.out.println(sqrt(263169));
    		System.out.println(sqrt(76123));
 	}  
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int absx2 = abs(x2);
+		for	 (int i = 0; i < absx2; i++) {
+		if (x2<0) { // adding a negative number
+			x1--;
+		}
+		else {
+			x1++;
+		}
+		}
+		return x1;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int absx2 = abs(x2);
+		for (int i = 0; i < absx2 ; i++) {
+			if (x2<0) { // subtracting a negative number
+				x1++;
+			}
+			else {
+				x1--;
+			}
+		}
+		return x1;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int absx1 = abs(x1);
+		int absx2 = abs(x2);
+		int product = 0;
+		if (x1 == 0 || x2 == 0) { // multiplication by zero
+			return 0;
+		}
+		for (int i = 0; i < absx2; i++) { // adding abs(x1), abs(x2) times
+			product = plus(product, absx1);
+		}
+		if ((x1 < 0 && x2 > 0) || (x1 > 0 && x2 < 0)) { // product is negative
+			product = minus(0, product);
+		}		
+		return product;
 	}
 
-	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+		int absx = abs(x);
+		int result = 1;
+		if (n==0) { // x^0 = 1
+		return 1;
+		}
+		else if (x==0) { // 0^n = 0, n>0
+			return 0;
+		}
+		for (int i = 0; i < n; i++) { // multiply abs(x), n times
+			result = times(result, absx);
+		}
+		if (x<0 && mod(n, 2) == 1) { // x is negative and n is odd
+			result = minus(0, result);
+		}
+		return result;
 	}
 
-	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		if (x2 == 0) { // error: division by zero
+			return -1;
+		}
+		else if (x2 == 1) { // division by one
+			return x1;
+		}
+		else if (x2 == -1) { // division by minus one
+			return minus(0, x1);
+		}
+
+		int quotient = 0;
+		int divider = abs(x1);
+		int divisor = abs(x2);
+
+		while (divider != 0 && divider >= divisor) { // subtract divisor from divider until divider < divisor
+        divider = minus (divider, divisor);
+		quotient++;
+		}
+		if ((x1<0 && x2>0) || (x1>0 && x2<0)) { // quotient is negative
+			quotient = minus(0, quotient);
+			
+		}
+		return quotient;
 	}
 
-	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}	
+		int remainder = 0;
+		if (x1==0) {
+			return 0;
+		}
+		if (x2 == 0) { // error: modulo by zero
+	    return -1; 
+		}
+		remainder = minus(x1,times(div(x1, x2), x2)); // r = a1 - q*n, by definition of modulo
+		return remainder;
+	}
 
-	// Returns the integer part of sqrt(x) 
-	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
-	}	  	  
+ 	public static int sqrt(int x) {
+		if (x == 0) {
+			return 0;
+		}
+		else if (x == 1) {
+			return 1;
+		}
+		int root = 0;
+		int square = 0;
+		while (square <= x) { // increment root until root^2 > x
+			root++;
+			square = times(root, root);
+		}
+		return minus(root, 1);
+		
+	}
+
+	public static int abs(int x) { // returns the absolute value of x
+		int inverse = 0;
+		if (x < 0) {
+			for (int i = x; i < 0; i++) { // return -x
+			   inverse++;
+			}
+			return inverse;
+		}
+		else {
+			return x;
+		}
+	}
 }
